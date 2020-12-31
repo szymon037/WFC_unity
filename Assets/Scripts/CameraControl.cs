207c4d86728fd7c9714a741b0ecd728140c4c4c3
Szymon Omieciński <szymon.lost@gmail.com>
2020-12-31 01:33:25 +0100

    Added tiles builder

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float rotationAngle = 20f;
+
+    private Transform t;
+    private float lastMousePosX = 0;
+
+    void Start()
+    {
+        t = transform;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(2))
+        {
+            float currentMousePosX = Input.mousePosition.x;
+            float delta = currentMousePosX - lastMousePosX;
+            t.Rotate(Vector3.up, rotationAngle * delta);
+            lastMousePosX = currentMousePosX;
+        }
+
+        lastMousePosX = Input.mousePosition.x;
+
+    }
+}
